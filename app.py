fd84e143e58c9041ef4cb230180238dcd7c63a8a
abhijith2710 <72353125+abhijith2710@users.noreply.github.com>
2022-07-06 11:08:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,19 +33,8 @@ _max_width_()
 c30, c31, c32 = st.columns([2.5, 1, 3])
 
 with c30:
-    col1, col2, col3 = st.beta_columns([1,6,1])
-
-    with col1:
-        st.write("")
-
-    with col2:
-        st.image("logo.png", width=400)
-
-    with col3:
-        st.write("")
-    
+    st.image("logo.png", width=400)
     st.header("")
-
     st.markdown("")
 
 st.markdown("")
